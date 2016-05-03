5c78d2d89d2ef91f6ecba77543333519f21cf5ae
craigy <craigy@users.noreply.github.com>
2016-05-02 22:08:14 -0400

    Start working on imdb data parser.

---
+++ imdb-parser/src/clj/imdb_parser/core.clj
@@ -0,0 +1,42 @@
+(ns imdb-parser.core
+  (:require
+    [boot.cli :as cli]
+    [compojure.core :refer [defroutes routes ANY GET]]
+    [compojure.route :refer [resources not-found]]
+    [ring.middleware.params :refer [wrap-params]]
+    [ring.util.response :as r]
+    [org.httpkit.server :as h])
+  (:gen-class))
+
+
+(def index
+  (->
+    (r/resource-response "index.html" {:root "public"})
+    (r/header "Content-Type" "text/html; charset=utf-8")))
+
+(defn count-lines [request]
+  (with-open [rdr (clojure.java.io/reader "/home/craigy/Downloads/quotes.list")]
+    (str (count (line-seq rdr)))))
+
+(defroutes http-routes
+  (resources "/")
+  (resources "/public")
+  (resources "/" {:root "/META-INF/resources"})
+  (ANY "/" [] index)
+  (GET "/count" [] count-lines)
+  (not-found "404"))
+
+(def handler
+  (-> (routes
+        http-routes)
+      wrap-params))
+
+(defn init []
+  (do
+    (println "Initializing")))
+
+(defn -main [& args]
+  (let [port (if (seq args) (read-string (first args)) 3000)]
+    (h/run-server handler {:port port})
+    (println "Started server on port" port)))
+
